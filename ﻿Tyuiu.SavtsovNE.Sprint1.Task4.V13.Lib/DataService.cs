b3a5e137f7a72f23b16119f5d24c310488efd3ab
Никита Савцов <nsavtsov89@inbox.ru>
2024-10-02 21:58:19 +0500

    Task4

--- ﻿Tyuiu.SavtsovNE.Sprint1.Task4.V13.Lib/DataService.cs
+++ ﻿Tyuiu.SavtsovNE.Sprint1.Task4.V13.Lib/DataService.cs
@@ -1,15 +1,16 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint1;
 using System;
-namespace Tyuiu.SavtsovNE.Sprint1.Task4.V13.Lib;
+namespace Tyuiu.SavtsovNE.Sprint1.Task4.V13.Lib
 
-public class DataService : ISprint1Task4V13
-{
-    public double Calculate(double x, double y)
+{public class DataService : ISprint1Task4V13
     {
-        double result = Math.Cos(Math.PI / x) / (3 * Math.Exp(x) + y);
+        public double Calculate(double x, double y)
+        {
+            double result = Math.Cos(Math.PI / x) / (3 * Math.Exp(x) + y);
 
-        result = Math.Round(result, 3);
+            result = Math.Round(result, 3);
 
-        return result;
+            return result;
+        }
     }
 }
