1acf4d309344a0c8dc7070e99038408332afa77e
Никита Савцов <nsavtsov89@inbox.ru>
2024-10-03 23:13:38 +0500

    task3

---
+++ Tyuiu.SavtsovNE.Sprint1.Task3.V5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Transactions;
+using tyuiu.SavtsovNE.Sprint1.Task3.V5.Lib;
+
+namespace tyuiu.SavtsovNE.Sprint1.Task3.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Савцов Никита Евгеньевич | ИБКСБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил:Савцов Никита Евгеньевич  | ИБКСБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ                                                                 *");
+            Console.WriteLine("* Написать программу вычисления расстояния между населёнными пунктами,    *");
+            Console.WriteLine("* изображенными на карте.                                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double a = 120;
+            double b = 3.5;
+            Console.WriteLine("Масштаб карты = " + a);
+            Console.WriteLine("Расстояние между точками, изображающими населенные пункты = " + b);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Расстояние между населенными пунктами = " + ds.DistanceLength(a, b));
+
+            Console.ReadKey();
+
+        }
+    }
+}
