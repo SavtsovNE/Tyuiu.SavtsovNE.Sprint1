5633810f65f828d18a0fefe3d19e75bee5437577
Никита Савцов <nsavtsov89@inbox.ru>
2024-10-03 21:51:44 +0500

    Task5

---
+++ Tyuiu.SavtsovNE.Sprint1.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SavtsovNE.Sprint1.Task5.V3.Lib;
+
+namespace Tyuiu.SavtsovNE.Sprint1.Task5.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+
+            DataService ds = new DataService( );
+            int k = 15435;
+            int wait = 4;
+            var res = ds.Calculate(k);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
