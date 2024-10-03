96d28548c652ea3b8ef507f0598ad699c7dcd0fb
Никита Савцов <nsavtsov89@inbox.ru>
2024-10-03 23:04:02 +0500

    task3

--- Tyuiu.SavtsovNE.Sprint1.Task3.V5.Lib/DataService.cs
+++ Tyuiu.SavtsovNE.Sprint1.Task3.V5.Lib/DataService.cs
@@ -2,7 +2,7 @@
 
 namespace tyuiu.SavtsovNE.Sprint1.Task3.V5.Lib
 {
-    public class DataService : ISprint1Task3V5 
+    public class DataService : ISprint1Task3V5
     {
         public double DistanceLength(double mapScale, double distanceBetweenPoints)
         {
